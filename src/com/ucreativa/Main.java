6b65b7f074ed4794630390c6b6363ca33aae7de0
KennethCorCreativa <kenneth.cordero.orozco@ucreativa.com>
2021-05-24 18:38:30 -0600

    Hola Clase first commit

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -3,6 +3,7 @@ package com.ucreativa;
 public class Main {
 
     public static void main(String[] args) {
-           System.out.println(¨Hola Clase¨)
+           System.out.println("Hola Clase");
+
     }
 }
