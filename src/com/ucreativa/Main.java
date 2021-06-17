647d193fd4cd0e243e3f4fa922d45b65839e6264
KennethCorCreativa <kenneth.cordero.orozco@ucreativa.com>
2021-06-16 23:49:16 -0600

    UI

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -8,6 +8,7 @@ import com.ucreativa.vacunacion.repositories.FileRepository;
 import com.ucreativa.vacunacion.repositories.InMemoryRepository;
 import com.ucreativa.vacunacion.repositories.Repository;
 import com.ucreativa.vacunacion.services.BitacoraService;
+import com.ucreativa.vacunacion.ui.FrontEnd;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,52 +20,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        BitacoraService service = new BitacoraService(new FileRepository());
+        FrontEnd fe = new FrontEnd("Sistema de Vacunación");
+        fe.build();
 
-        String nombre, cedula, edad, riesgo, isAmigo, relacion = "", facebook = "", parentesco = "", marca, print;
-
-        while(true){
-
-        Persona persona;
-
-        System.out.println("Nombre:");
-        nombre = in.nextLine();
-        System.out.println("Cedula:");
-        cedula = in.nextLine();
-        System.out.println("Edad:");
-        edad = in.nextLine();
-        System.out.println("Riesgo(S/N):");
-        riesgo = in.nextLine();
-        System.out.println("Amigo(A)/Familiar(F):");
-        isAmigo = in.nextLine();
-        if (isAmigo.equals("A")){
-            System.out.println("Relacion:");
-            relacion = in.nextLine();
-            System.out.println("Facebook:");
-            facebook = in.nextLine();
-
-        } else {
-            System.out.println("Parentesco:");
-            parentesco = in.nextLine();
-
-
-        }
-        System.out.println("Vacuna -- Marca:");
-        marca = in.nextLine();
-
-        service.save(nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca);
-
-        System.out.println("Quiere imprimir Lista (S)");
-        print = in.nextLine();
-        if (print.equals("S")){
-            for(String item :service.get()){
-                System.out.println(item);
-            }
-        }
-
-
-    }
     }
 
 }
